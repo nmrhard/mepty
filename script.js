8c0fa67dc582920bdbd25f63a718a68d506cd449
nmr <nmrhard@gmail.com>
2021-02-24 23:18:27 +0200

    Displaying  a Map Marker

--- script.js
+++ script.js
@@ -25,12 +25,26 @@ if (navigator.geolocation) {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            L.marker(coords).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            map.on('click', function(mapEvent) {
+                const {lat, lng} = mapEvent.latlng;
+
+                L.marker([lat, lng] )
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                    })
+                )
+                .setPopupContent('Workout')
                 .openPopup();
-                    },
-                    function() {
-                        alert('Could not get your position');
-                    }
+            });
+        },
+        function() {
+            alert('Could not get your position');
+        }
     );
 }
